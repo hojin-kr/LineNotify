ec3781a29269667dc28a74160695732311aa3f27
hojin <jhj377@gmail.com>
2021-09-27 01:45:29 +0900

    Update start.sh

--- attend/start.sh
+++ attend/start.sh
@@ -12,9 +12,9 @@ STICKER_ID="10856"
 MESSAGE=$(date "+%d일 %H시 %M분 %S초")
 
 echo $(date "+%d") > check0
-echo $(date -d +8hour "+%d%H%M") > check1
-echo $(date -d +9hour "+%d%H%M") > check2
-echo $(date -d +7hour "+%d%H%M") > check3
+echo $(date -d +8hour "+%d%H%M") > /tmp/check1
+echo $(date -d +9hour "+%d%H%M") > /tmp/check2
+echo $(date -d +7hour "+%d%H%M") > /tmp/check3
 
 curl -X POST -H "Authorization: Bearer ${TOKEN}" -F "message=${MESSAGE}" -F "stickerPackageId=${STICKER_PACKAGE_ID}" -F "stickerId=${STICKER_ID}" \
 https://notify-api.line.me/api/notify
